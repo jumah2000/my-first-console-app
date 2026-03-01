f8e82b92d1b8b73a5b05a4722858dd427b33b6f4
Mustapha Ajimo Aduke <abbeymustapha45@gmail.com>
2026-03-01 09:44:24 +0100

    my first console app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Hello World");
+//Console.WriteLine("My name is Jumah");
+
+//Variable Declaration(int, double, string, boot)
+
+int age = 50;
+string name = "Jumah";
+double height = 34.5;
+bool ispresent = false;
+/////////////////////////
+Console.WriteLine(name + " " + height);
+Console.WriteLine($"{name} {height}");
+Console.WriteLine($"my name  is {name}, i am {age} years old. my height is {height}. are you a christain: {ispresent}");
+Console.WriteLine("my name is " + name + "i am " + age + " years old. my height is " + height + ". are you a christian: " + ispresent);
