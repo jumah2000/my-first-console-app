c62523e357f0c04aa72fc76458a8af93e5133237
Mustapha Ajimo Aduke <abbeymustapha45@gmail.com>
2026-03-07 13:06:25 +0100

    Completed classwork March 7st

--- Program.cs
+++ Program.cs
@@ -1,78 +1,41 @@
-﻿//Console.WriteLine("Hello World");
-//Console.WriteLine("My name is Jumah");
+﻿//Task 1- Arithmetic Operators
 
-//Variable Declaration(int, double, string, boot)
+//int a = 60;
+//int b = 25;
+//int addedResult = a + b;
+//int subtractionResult = a - b;
+//int multiplicationResult = a * b;
+//double divisionResult = (double)a / b;
+//int modulusResult = a % b;
+//Console.WriteLine($"adding a and b value = {addedResult}");
+//Console.WriteLine($"subtracting a and b value = {subtractionResult}");
+//Console.WriteLine($"multiplying a and b value = {multiplicationResult}");
+//Console.WriteLine($"dividing a and b value = {divisionResult}");
+//Console.WriteLine($"modulus a and b value = {modulusResult}");
 
-int age = 50;
-string name = "Jumah";
-double height = 34.5;
-bool ispresent = false;
-/////////////////////////
-//Console.WriteLine(name + " " + height);
-//Console.WriteLine($"{name} {height}");
-//Console.WriteLine($"my name  is {name}, i am {age} years old. my height is {height}. are you a christain: {ispresent}");
-//Console.WriteLine("my name is " + name + "i am " + age + " years old. my height is " + height + ". are you a christian: " + ispresent);
+//Task 2- Integer vs Double
+int a = 10;
+int b = 4;
+int integerDivisionResult = a / b;
+double doubleDivisionResult = (double)a/b;
+Console.WriteLine($"Dividing a and b value = {integerDivisionResult} integer division result");
+Console.WriteLine($"Dividing a and b value = {doubleDivisionResult} double division result");
 
-decimal money = 100.35m;
-float temperature = 30.5f;
-char grade = 'f';
-long pop = 2147483647;
+//Task 3- Large Numbers
+long pop = 90000000;
+int increase = 250000;
+long newPopResult = pop + increase;
+Console.WriteLine($"Adding population and increasing it = {newPopResult} new population");
 
-//int , memory 4byte, whole number e.g 10, 20 ,30
-//float, memory 4byte, decimal number (less precision) e.g 10.5f, 20.5f
-//double, memory 8byte, decimal number e.g 10.564, 15.10
-//decimal, memory 16byte, very precise value (money) e.g bank acct number
-//long, memory 8byte, very large number e.g 6000000000, 700000000
+//Task 4 - shop Calculator 
+decimal price = 1500.75m;
+int quantity = 3;
+decimal totalPriceResult= price * quantity;
+Console.WriteLine($"Total price = {totalPriceResult}");
 
-//mas and nin for -2,147,648 and 2,2147,484,647
-
-//subject: Operator
-
-//  we have four operator : *, +, -, /
-
-int a = 45;
-int b = 40;
-int addedResult = a + b;
-int subtractResult = a - b;
-int multiplyResult = a * b;
-double divideResult =(double) a / b;
-//Console.WriteLine($"Adding a and b value = {addedResult} ");
-//Console.WriteLine($"subtracting a and b value = {subtractResult}");
-//Console.WriteLine($"multiplying a and b value = {multiplyResult}");
-//Console.WriteLine($"dividing a and b value = {divideResult}");
-
-int pop1 = 200000;
-long pop2 = 9000000000;
-long popResult = pop1 + pop2;
-//Console.WriteLine($"population tota1 = {popResult}"); // no precision
-
-int jumahBalance = 100;
-double sweetPrice = 5.75;
-double jumahamountleft = jumahBalance + sweetPrice;
-//Console.WriteLine($"jumah total = {jumahamountleft}"); // no precision
-
-float value1 = 5.234534635634f;
- decimal value2 = 2.5m;
- //decimal valueResult = (decimal)value1 + value2;
-float valueResult = value1 + (float)value2;
- //Console.WriteLine($"Lekan said the total value added = {valueResult}");
- 
- double acctbalance = 10.3475;
- decimal acctbalance2 = 5.20m;
-double acctresult = acctbalance + (double)acctbalance2;
- //Console.WriteLine($"Mariam said the total value added ={acctresult}");
- 
- //Easy rules
- //int + long => safe 
- //int + double => safe 
- //float + double => some precision loss
- //double + decimal => conversion need for any variable type we want
-//float + decimal => conversion need for any variable type we want
-//decimal => best for money
- 
- //Examples
- int ab = 10;
- int bc = 5;
- int reminder = ab % bc;
-Console.WriteLine($"reminder values = {reminder}");
- 
+//Task 5 - Bonus Challenge
+int num1 = 50;
+int num2 = 10;
+int num3 = 5;
+ double averageResult = (num1 + num2 + num3) /3.0;
+ Console.WriteLine($"Average result: {averageResult}");
